8de759c5a30a3c883c3e7a2cd24c80c6b72c2331
Keido Sumi <delta0726@nta.pial.jp>
2022-03-10 08:06:06 +0900

    Add chap2 execute-streamlit

---
+++ udemy/pythonstreamlit/chap2_Introduction/1_execute_streamlit.py
@@ -0,0 +1,32 @@
+# ******************************************************************************
+# Course      : Python/Streamlitによる機械学習WEBアプリの開発
+# Chapter     : 2 Streamlit入門
+# Theme       : Streamlitの実行
+# Creat Date  : 2022/3/10
+# Final Update:
+# URL         : https://www.udemy.com/course/pythonstreamlit/
+# ******************************************************************************
+
+
+# ＜ポイント＞
+# - streamlitは｢.py｣ファイルで作成する（ファイル名には制約がない）
+# - ターミナルから起動する
+
+
+# ＜実行：ターミナル＞
+# conda activate pythonstreamlit
+# streamlit run .\chap2_Introduction\08_execute_streamlit.py
+
+
+# ＜停止：ターミナル＞
+# Ctrl + C
+
+
+# ライブラリ
+import streamlit as st
+
+# ページレイアウト
+st.set_page_config(layout='wide')
+
+# タイトル
+st.title('タイトル')
