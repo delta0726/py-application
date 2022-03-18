bf7282dbff27b0804ead4eb24ec37a5c9e14b70b
Keido Sumi <delta0726@nta.pial.jp>
2022-03-19 07:50:54 +0900

    Revert "Add Chap1 1_about"
    This reverts commit 6849d0b6dbd230656e44ea20000b7748b4683388.
    
    # Conflicts:
    #	book/plotly-dash-book/practice/ch01_plotly_express/1-1_about.py

--- book/plotly-dash-book/practice/ch01_plotly_express/1-2_Introduction.py
+++ book/plotly-dash-book/practice/ch01_plotly_express/1-2_Introduction.py
@@ -2,7 +2,7 @@
 # Book        : Pythonインタラクティブデータビジュアライゼーション入門
 # Chapter     : 1 Plotly Express
 # Theme       : 1-2 Plotly Expressとは
-# Creat Date  : 2022/3/17
+# Create Date : 2022/3/17
 # Final Update:
 # URL         : https://github.com/plotly-dash-book/plotly-dash-book
 # ******************************************************************************
@@ -13,8 +13,7 @@
 
 
 # ＜実行＞
-# conda activate plotly-dash-book
-# streamlit run .\practice\ch01_plotly_express\1-2_Introduction.py
+# VS-Code
 
 
 # ＜目次＞
@@ -29,62 +28,23 @@
 
 # ライブラリ
 import plotly.express as px
-import matplotlib.pyplot as plt
-import streamlit as st
+import pandas as pd
 
 
 # データ準備
-df = px.data.gapminder()
-
-# データ確認
-df.head()
-df.info()
+tips = px.data.tips()
+tips.head()
 
 
-# 1 プロット作成 --------------------------------------------------------------
+# 1 データの描画 --------------------------------------------------------------
 
-# プロット作成
-df.plot.scatter(x="gdpPercap", y="lifeExp", logx=True, xlim=[100, 1e6])
-# plt.show()
+# データ作成
+df = pd.DataFrame([[1, 1], [2, 5], [3, 3]], columns=["x", "y"])
 
-
-# 2 インタラクティブ・プロット作成 ------------------------------------------------
-
-# 散布図（ベーシック）
-fig1 = px.scatter(df, x="gdpPercap", y="lifeExp", log_x=True, hover_name="country")
-
-# 散布図（装飾あり）
-fig2 = px.scatter(df, x="gdpPercap", y="lifeExp", log_x=True,
-                  hover_name="country", size="pop",  size_max=40,  color="continent")
-
-# プロット表示
-st.plotly_chart(fig1)
-st.plotly_chart(fig2)
+#
+px.line(df, x="x", y="y").show()
 
 
-# 3 ファセット・プロット -----------------------------------------------------------
-
-# プロット作成
-facet_fig = px.scatter(df,x="gdpPercap", y="lifeExp", log_x=True,
-                       hover_name="country", size="pop", size_max=40,
-                       color="continent", facet_col="continent", width=800)
-
-# プロット更新
-facet_fig.update_xaxes(tickfont={"size": 8})
-
-# プロット表示
-st.plotly_chart(facet_fig)
+# 2 データ型 -----------------------------------------------------------------
 
 
-# 4 アニメーションプロット --------------------------------------------------------
-
-# プロット作成
-animation_fig = px.scatter(df, x="gdpPercap", y="lifeExp", log_x=True,
-                           hover_name="country", size="pop", size_max=40, color="continent",
-                           facet_col="continent", width=800, animation_frame="year")
-
-# プロット更新
-animation_fig.update_xaxes(tickfont={"size": 8})
-
-# プロット表示
-st.plotly_chart(animation_fig)
