d0e8f78fb39ff287b0f760ce98377b384865c493
Keido Sumi <delta0726@nta.pial.jp>
2022-03-20 17:09:13 +0900

    Add 6-1-2

---
+++ book/plotly-dash-book/practice/ch06_dash_layout/6-1-2_component_property.py
@@ -0,0 +1,34 @@
+# ******************************************************************************
+# Book        : Pythonインタラクティブデータビジュアライゼーション入門
+# Chapter     : 6 Dashレイアウト
+# Theme       : 1-2 コンポーネントの主要な属性
+# Creat Date  : 2022/3/20
+# Final Update:
+# Page        : P105
+# URL         : https://github.com/plotly-dash-book/plotly-dash-book
+# ******************************************************************************
+
+
+# ＜概要＞
+# - 各コンポーネントで共通して用いられる属性(プロパティ)を確認する
+
+
+# ＜主要な属性＞
+# - Children  : Divクラスでリストに格納した複数コンポーネントを指定
+# - id        : コンポーネントの名前（コールバックで使用）
+# - className : HTMLのclassグローバル属性 （Pythonのクラスとは異なる）
+# - options   : Dropdownなどのウィジェットの要素を指定
+# - style     : スタイル設定（辞書にCSSを格納して渡す）
+# - value     : コンポーネントの値
+
+
+# ＜ヘルプ確認＞
+# dcc.Dropdownにカーソールをあてる
+
+import dash
+import dash_core_components as dcc
+
+dcc.Dropdown(options=[],
+             value="red",
+             clearable=False,
+             style={"textAlign": "center"})
