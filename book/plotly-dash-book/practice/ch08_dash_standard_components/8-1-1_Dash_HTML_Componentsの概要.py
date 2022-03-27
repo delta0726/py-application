7362ae78e12de1ab10e4f2f523476d728d4b4829
Keido Sumi <delta0726@nta.pial.jp>
2022-03-27 11:14:27 +0900

    Add 8-1-1

---
+++ book/plotly-dash-book/practice/ch08_dash_standard_components/8-1-1_Dash_HTML_Componentsの概要.py
@@ -0,0 +1,23 @@
+# ******************************************************************************
+# Book        : Pythonインタラクティブデータビジュアライゼーション入門
+# Chapter     : 8 Dashの標準コンポーネント
+# Theme       : 1 Dash HTML Components
+# Topic       : 1 Dash HTML Componentsの概要
+# Update Date : 2022/3/27
+# Page        : P152
+# URL         : https://github.com/plotly-dash-book/plotly-dash-book
+# ******************************************************************************
+
+
+# ＜概要＞
+# - Dash_HTML_Componentsは、HTMLタグをPythonのクラスからラップして提供している
+#   --- Python記法でHTMLの操作をできるようにしている
+
+
+# ＜主要コンポーネント＞
+# - Div   ： コンテンツ分割
+# - H1～H6： 見出し
+# - P     ： テキストの段落
+# - Link  ： リンク
+# - Button： ボタン
+# - Img   ： 画像埋め込み
