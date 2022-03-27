96f896c14f85f9f1f202266f9416ef33ebbd1ff1
Keido Sumi <delta0726@nta.pial.jp>
2022-03-27 14:07:05 +0900

    Add 8-2-1

---
+++ book/plotly-dash-book/practice/ch08_dash_standard_components/8-2-1_Dropdownコンポーネント_4.py
@@ -0,0 +1,67 @@
+# ******************************************************************************
+# Book        : Pythonインタラクティブデータビジュアライゼーション入門
+# Chapter     : 8 Dashの標準コンポーネント
+# Theme       : 2 Dash Core Component
+# Topic       : 1 Dropdownコンポーネント
+# Update Date : 2022/3/27
+# Page        : P155 - P158
+# URL         : https://github.com/plotly-dash-book/plotly-dash-book
+# ******************************************************************************
+
+
+# ＜概要＞
+# - チェックリストの作成
+#   --- ドロップダウンリストと同様のオブジェクト構成
+
+
+# ＜実行＞
+# conda activate plotly-dash-book
+# python .\practice\ch08_dash_standard_components\8-2-1_Dropdownコンポーネント_4.py
+
+
+# ＜目次＞
+# 0 準備
+# 1 レイアウトの作成
+# 2 アプリの起動
+
+
+# 0 準備 ---------------------------------------------------------------------
+
+# ライブラリ
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly
+import plotly.express as px
+
+# データロード
+gapminder = plotly.data.gapminder()
+
+# インスタンス生成
+app = dash.Dash(__name__)
+
+
+# 1 レイアウトの作成 -----------------------------------------------------------
+
+app.layout = html.Div(
+    [
+        dcc.Checklist(
+            options=[
+                {"label": "東京", "value": "東京"},
+                {"label": "北海道", "value": "北海道"},
+                {"label": "静岡", "value": "静岡"},
+                {"label": "愛知", "value": "愛知"},
+                {"label": "京都", "value": "京都"},
+            ],
+            value=["京都"],
+            style={"textAlign": "center"}
+        )
+    ],
+    style={"width": "80%", "margin": "3% auto"}
+)
+
+
+# 2 アプリの起動 ---------------------------------------------------------------
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
